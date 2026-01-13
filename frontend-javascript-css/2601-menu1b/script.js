fc22454d9d16bacb163760b6c47fdb14d70a5958
zicox <zicox@users.noreply.github.com>
2026-01-13 19:29:51 +0100

    version ts

---
+++ frontend-javascript-css/2601-menu1b/script.js
@@ -0,0 +1,6 @@
+function crearNodo(tipo) {
+    var ele = document.createElement(tipo);
+    document.body.appendChild(ele);
+    return ele;
+}
+console.log('prueba de typescript');
---
+++ frontend-javascript-css/2601-menu1b/script.ts
@@ -0,0 +1,22 @@
+function iniciar(){
+
+}
+
+function generarTexto(caracteres: number){
+    let letras: string = 'qwertyuiopaasdfghjklñzxcvbnm';
+
+    let min: number = 0;
+    let max: number = letras.length - 1;
+    let resultado: string = '';
+
+    
+
+}
+
+function crearNodo(tipo: string){
+    var ele = document.createElement(tipo);
+    document.body.appendChild(ele);
+    return ele;
+}
+
+window.onload = iniciar;
