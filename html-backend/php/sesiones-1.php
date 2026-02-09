bbc83796b34d04b405933fedf62cbb4b0aebb476
debian-zicox <debian-zicox@github.com>
2026-02-09 20:53:07 +0100

    sesiones

---
+++ html-backend/php/sesiones-1.php
@@ -0,0 +1,33 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        div {
+            display: block;
+        }
+    </style>
+</head>
+<body>
+    <form action="" method="post">
+        <div>
+
+            <label for="">Sesion: </label>
+            <select name="sesion" id="">
+                <option value="1">Sesion 1</option>
+                <option value="2">Sesion 2</option>
+            </select>
+        </div>
+        <div>
+            <input type="text" name="contenido">
+        </div>
+        <input type="submit">
+    </form>
+</body>
+</html>
